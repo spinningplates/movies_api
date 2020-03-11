035f11639be276122ee0b24dfffdbfcf184938da
Keegan Galea <keegangalea@Keegans-MacBook-Pro.local>
2020-03-11 11:18:30 -0600

    Adding MoviesController

---
+++ MoviesAPI/Controllers/MoviesController.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MoviesAPI.Data;
+using MoviesAPI.Models;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MoviesAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [EnableCors("AllowOrigin")]
+    public class MoviesController : ControllerBase
+    {
+        private readonly AppDbContext _db;
+
+        public MoviesController(AppDbContext db)
+        {
+            _db = db;
+        }
+
+        [HttpGet]
+        public IActionResult GetEntries()
+        {
+            try
+            {
+                return Ok(_db.Movies.ToList());
+            }
+            catch (Exception ex)
+            {
+                return new JsonResult(ex.Message);
+            }
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetMovie([FromRoute] int id)
+        {
+            try
+            {
+                var movie = _db.Movies.FirstOrDefault(n => n.Id == id);
+
+                if (movie == null)
+                {
+                    return NotFound();
+                }
+                return Ok(movie);
+            }
+            catch (Exception ex)
+            {
+                return new JsonResult(ex.Message);
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddMovie([FromBody] Movie objMovie)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                {
+                    return new JsonResult("Error while creating new entry.");
+                }
+
+                _db.Movies.Add(objMovie);
+                await _db.SaveChangesAsync();
+                return new JsonResult("Movie was added.");
+            }
+            catch (Exception ex)
+            {
+                return new JsonResult(ex.Message);
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteMovie([FromRoute] int id)
+        {
+            try
+            {
+                var findMovie = await _db.Movies.FindAsync(id);
+
+                if (findMovie == null)
+                {
+                    return NotFound();
+                }
+
+                _db.Movies.Remove(findMovie);
+                await _db.SaveChangesAsync();
+
+                return new JsonResult("Entry was successfully deleted.");
+            }
+            catch (Exception ex)
+            {
+                return new JsonResult(ex.Message);
+            }
+        }
+    }
+}
